51b123f3bc8e110e7a3492c81a7435738a3105e3
LWagg <109525643+LWagg@users.noreply.github.com>
2022-12-09 15:11:23 -0300

    Quiz!

---
+++ assets/app.js
@@ -0,0 +1,78 @@
+const form = document.querySelector('form')
+const result = document.querySelector('.result')
+const button = document.querySelector('button')
+const reset = document.querySelector('#reset')
+
+const correctAnswers = ['B', 'C', 'D', 'A', 'B']
+
+let score = 0
+
+const getUserAnswers = () => {
+    let userAnswers = []
+
+    correctAnswers.forEach((_, index) => {
+        const userAnswer = form[`inputQuestion${index + 1}`].value
+        userAnswers.push(userAnswer)
+    })
+
+    return userAnswers
+}
+
+const calculateUserScore = userAnswers => {
+    userAnswers.forEach((userAnswer, index) => {
+        const isUserAnswerCorrect = userAnswer === correctAnswers[index]
+
+        if (isUserAnswerCorrect)
+        score += 20
+    })
+}
+
+const showFinalScore = () => {
+    scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+    result.style.display = 'block'
+}
+
+const animateFinalScore = () => {
+    let counter = 0
+
+    const timer = setInterval(() => {
+        if (counter === score) {
+            clearInterval(timer)
+        }
+        result.querySelector('span').textContent = `${counter++}%`
+    }, 10)
+}
+
+const hideSubmitButton = () => {
+    button.style.display = 'none'
+}
+
+const resetPage = () => {
+    reset.style.display = 'block'
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    
+    const userAnswers = getUserAnswers()
+     
+    
+
+    calculateUserScore(userAnswers)
+    showFinalScore()
+    animateFinalScore()
+    resetPage()
+    hideSubmitButton()
+    
+})
+
+reset.addEventListener('click', () => {
+    scrollTo(0, 0)    
+    document.location.reload()
+        
+})
+
